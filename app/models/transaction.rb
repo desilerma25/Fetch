901dcb78d89f81095a7375c858f200ca0914c60c
Desiree Lerma <desi.lerma96@yahoo.com>
2021-09-01 21:22:25 -0500

    Adds method to get total sum of points.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,25 @@
 class Transaction < ApplicationRecord
+
+    def self.total_points
+        total = []
+        self.all.each do |t|
+            total << t.points
+        end
+        return total.sum
+    end
+
+    def self.spend_points(num) 
+
+        # dont drop below neg
+        # if # all transaction points does not eq neg
+        #     # sort transactions by oldest 
+        #     # subtract until 0
+        #     # move to next trans until points is 0
+        #     # return how array of subtracted points and from what payer
+        # else
+        #     "Sorry! You do not appear to have enough points."
+        # end
+        # spend from oldest to newest date
+        # return list of points spent
+    end
 end
