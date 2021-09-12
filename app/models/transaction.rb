99b7939145bcdf4ab9fc5d3924fd4a52531975d7
Desiree Lerma <desi.lerma96@yahoo.com>
2021-09-12 18:13:26 -0500

    Adds payer bal method to model. Removes unused code.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -11,7 +11,6 @@ class Transaction < ApplicationRecord
     def self.spend_points(spend)
         sorted = self.all.sort_by(&:date)
         used_points = {}
-        return_resp = []
         if self.total_points < spend
             print "Sorry, you do not appear to have enough points."
         else
@@ -23,29 +22,38 @@ class Transaction < ApplicationRecord
                 if sorted[i].points > 0
                     if spend - sorted[i].points >= 0
                         used_points[sorted[i].payer] = -1 * sorted[i].points
-                        # used_points["payer"] = sorted[i].payer
-                        # used_points["points"] = -1 * sorted[i].points
                         spend = spend - sorted[i].points
                         Transaction.update(sorted[i].id, :points => 0)
                     else
                         remaining = sorted[i].points - spend
                         used_points[sorted[i].payer] = -1 * spend
-                        # used_points["payer"] = sorted[i].payer
-                        # used_points["points"] = -1 * spend
                         spend = 0
                         Transaction.update(sorted[i].id, :points => remaining)
                     end
                 else
                     spend = spend - sorted[i].points
                     used_points[sorted[i].payer] = -sorted[i].points
-                    # used_points["payer"] = sorted[i].payer
-                    # used_points["points"] = -sorted[i].points
                     Transaction.update(sorted[i].id, :points => 0)
                 end
                 i+= 1
             end
         end
         used_points
+    end
+
+    def self.payer_bal
+        transactions = self.all
+        balances = {}
+        i = 0
+        while i < transactions.length
+            if balances[transactions[i].payer] 
+                balances[transactions[i].payer] += transactions[i].points
+            else
+                balances[transactions[i].payer] = transactions[i].points
+            end
+            i+= 1
+        end
+        balances
     end
 
 end
