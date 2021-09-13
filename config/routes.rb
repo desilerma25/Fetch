b6fd756468c5bf91628fb7aa9111d7a8863d3699
Desiree Lerma <desi.lerma96@yahoo.com>
2021-09-13 01:16:10 -0500

    Updates routes. Removes view html files. Updates controller methods to render json.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'transactions#index'
-  resources :transactions
+
+  get '/payer_balances', to: 'transactions#index'
+  post '/add_transaction', to: 'transactions#create_t'
+  post '/spend_points', to: 'transactions#spend_pts'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
